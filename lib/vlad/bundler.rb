40636efa006c04138ae822f4dcf6e9912522541f
Pascal Houliston <101pascal@gmail.com>
2010-09-09 16:35:46 +0200

    Add option to specify gem install path for bundler

--- lib/vlad/bundler.rb
+++ lib/vlad/bundler.rb
@@ -1,8 +1,12 @@
 require 'vlad'
 
 namespace :vlad do
+  # Path that bundler should install gems to, set to false to install to system gems.
+  set :gem_install_path, 'vendor'
+
   desc "Ensure bundler is up to date, and install any missing dependencies."
   remote_task :bundler, :roles => :web do
-    run "cd #{current_path} && (bundle check || bundle install --without development test)"
+    path_option = (gem_install_path)? "--path #{gem_install_path}" : ''
+    run "cd #{current_path} && (bundle check || bundle install --without development test #{path_option})"
   end
 end
