823ecca63f2b8d2367d84885d6b98d9c768c36b7
Pascal Houliston <101pascal@gmail.com>
2010-09-09 15:12:57 +0200

    Add bundle check to avoid downloading source index every time

--- lib/vlad/bundler.rb
+++ lib/vlad/bundler.rb
@@ -3,6 +3,6 @@ require 'vlad'
 namespace :vlad do
   desc "Ensure bundler is up to date, and install any missing dependencies."
   remote_task :bundler, :roles => :web do
-    run "cd #{current_path} && bundle install --deployment --without development test"
+    run "cd #{current_path} && (bundle check || bundle install --deployment --without development test)"
   end
 end
