4dd617996ea9658cf59465c3a3a5c8b5c13b0b15
Pascal Houliston <101pascal@gmail.com>
2010-09-09 17:28:11 +0200

    Silence bundle check output

--- lib/vlad/bundler.rb
+++ lib/vlad/bundler.rb
@@ -6,6 +6,6 @@ namespace :vlad do
 
   desc "Ensure bundler is up to date, and install any missing dependencies."
   remote_task :bundler, :roles => :web do
-    run "cd #{current_path} && (bundle check || bundle install --without development test --path #{gem_install_path || shared_path+'/gems'})"
+    run "cd #{current_path} && (bundle check > /dev/null || bundle install --without development test --path #{gem_install_path || shared_path+'/gems'})"
   end
 end
