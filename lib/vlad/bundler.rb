15ac86633656dac606def0bcf34059a4291b70b5
Pascal Houliston <101pascal@gmail.com>
2010-09-09 14:34:35 +0200

    Add bundler task to vlad

---
+++ lib/vlad/bundler.rb
@@ -0,0 +1,8 @@
+require 'vlad'
+
+namespace :vlad do
+  desc "Ensure bundler is up to date, and install any missing dependencies."
+  remote_task :bundler, :roles => :web do
+    run "cd #{current_path} && (bundle check || bundle install --without development)"
+  end
+end
