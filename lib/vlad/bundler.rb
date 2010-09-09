9fc3914471e96c5ed25154db1421efaa536a3005
Pascal Houliston <101pascal@gmail.com>
2010-09-09 17:04:56 +0200

    Default bundler path to shared path

--- lib/vlad/bundler.rb
+++ lib/vlad/bundler.rb
@@ -2,11 +2,10 @@ require 'vlad'
 
 namespace :vlad do
   # Path that bundler should install gems to, set to false to install to system gems.
-  set :gem_install_path, 'vendor'
+  set :gem_install_path
 
   desc "Ensure bundler is up to date, and install any missing dependencies."
   remote_task :bundler, :roles => :web do
-    path_option = (gem_install_path)? "--path #{gem_install_path}" : ''
-    run "cd #{current_path} && (bundle check || bundle install --without development test #{path_option})"
+    run "cd #{current_path} && (bundle check || bundle install --without development test --path#{gem_install_path || shared_path+'/gems'})"
   end
 end
